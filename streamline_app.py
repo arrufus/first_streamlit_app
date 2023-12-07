3edf1bf06e97f3ff3b1606761058a55bad6e083e
arrufus <30685928+arrufus@users.noreply.github.com>
2023-12-07 15:47:17 +0000

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -57,7 +57,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 # streamlit.text("What fruit would you like to add?")
 # add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
